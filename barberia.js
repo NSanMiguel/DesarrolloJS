6b034c5cd4b09adc4dcdc181bf0ebec7e0f86d58
Nicolas San Miguel <nicolassanmiguel3@gmail.com>
2022-07-04 19:46:49 -0300

    barberia.js

--- barberia.js
+++ barberia.js
@@ -1,4 +1,7 @@
+
 let ingDia = 0
+
+let ingresarServicio
 
 let ingHora = 0
 
@@ -6,11 +9,21 @@ let total = 0
 
 const guardarDatos = []
 
-const servicios = []
+const servicios = [
+    {index: 1, servicio:'Corte', precio: 700},
+    {index: 2, servicio:'Cejas', precio: 200},
+    {index: 3, servicio:'Barba', precio: 300},
+    {index: 4, servicio:'Color', precio: 1500}, 
+]
+
+const agregarServicios=[]
+
+const servicios2 = ["Corte","Cejas","Barba","Color"]
 
 let ingNombre = prompt("Ingrese su nombre: ")
 
 let ingApellido = prompt("Ingrese su apellido: ")
+
 
 function menuEntrada(){
     let selecEntrada= prompt(" 1 - Lista de precios \n 2 - Solicitar turno")
@@ -26,22 +39,9 @@ function menuEntrada(){
     }
 }
 
-class tipoDeServicio {
-    constructor (servicio, precio){
-        this.servicio = servicio
-        this.precio = precio
-    }
-}
-
-servicios.push(new tipoDeServicio("Corte ",700))
-servicios.push(new tipoDeServicio("Cejas ",200))
-servicios.push(new tipoDeServicio("Barba ",300))
-servicios.push(new tipoDeServicio("Color ",1200))
-console.log(servicios)
-
 function mostrarServicio(){
     for(const servicio of servicios){
-        alert(servicio.servicio + servicio.precio + "$")
+        alert(servicio.servicio + " " + "$" + servicio.precio )
     }
 }
 
@@ -52,8 +52,8 @@ function listaPrecios(){
 
 class nuevoCliente {
     constructor (nombre,apellido){
-        this.nombre = nombre
-        this.apellido = apellido
+        this.nombre = nombre.toUpperCase()
+        this.apellido = apellido.toUpperCase()
     }
 }
 
@@ -65,13 +65,21 @@ function agregarCliente(){
 function continDia(){
     do {
         ingDia = prompt (ingNombre + ", Seleccione el dia que desea asistir \n 1 - Martes \n 2 - Miercoles \n 3 - Jueves \n 4 - Viernes \n 5 - Sabado")
+        if(ingDia > 5){
+            alert("Seleccione el dia correcto")
+            continDia()
+        }
     }while(ingDia <=0)
     
 }
 
 function continHora(){
     do {
-        ingHora = prompt ( ingNombre + ", Ingrese el horario en el que desea asistir : ")
+        ingHora = parseFloat(prompt ( ingNombre + ", Ingrese el horario en el que desea asistir : "))
+        if((ingHora > 21)|| (ingHora < 14)){
+            alert("Seleccione el horario correcto")
+            continHora()
+        }
     }while(ingHora <=0)
 }
 
@@ -79,6 +87,7 @@ function selecTurno(){
     switch(ingDia){
     case "1":
         alert ( ingNombre + ", Su turno es el dia martes a las " + ingHora + " horas")
+
         break;
     case "2":
         alert (ingNombre + ", Su turno es el dia miercoles a las " + ingHora + " horas")
@@ -98,16 +107,61 @@ function selecTurno(){
     }
 }
 
-function Turno(){
+class IngresarServicio {
+    constructor(tipoServicio,tipoPrecio){
+        this.tipoServicio = tipoServicio
+        this.tipoPrecio = tipoPrecio
+    }
+}
+
+function selectServicio(){
+    do {
+    seleccionarServicio = prompt("Seleccione el serivicio que desea: " + servicios2)
+    if(seleccionarServicio == "Corte"){
+        agregarServicios.push (new IngresarServicio (seleccionarServicio, 700))
+    }else if(seleccionarServicio == "Cejas"){
+        agregarServicios.push (new IngresarServicio (seleccionarServicio, 200))
+    }else if(seleccionarServicio == "Barba"){
+        agregarServicios.push (new IngresarServicio (seleccionarServicio, 300))
+    }else if(seleccionarServicio == "Color"){
+        agregarServicios.push (new IngresarServicio (seleccionarServicio, 1500))
+    }else
+        selectServicio()
+    }while(seleccionarServicio <=0)
+    otroServicio()
+}
+
+function calcularTotal (){
+    let totalservicio
+    totalservicio = agregarServicios.reduce ( (acumulador, service )  => acumulador + service.tipoPrecio , 0) 
+    alert("El total a pagar es " + "$" + totalservicio)
+}
+
+function otroServicio(){
+    do{
+        selectOtroServicio = prompt("Desea seleccionar otro servicio?\n Ingrese s/n ")
+        if(selectOtroServicio == "n"){
+            calcularTotal()
+            console.log(agregarServicios)
+            alert(ingNombre + ", gracias por solicitar su turno.")
+        }else if (selectOtroServicio == "s"){
+           selectServicio()
+        }else
+            otroServicio()
+        }while(selectOtroServicio == null)
+}
+
+function turno(){
     menuEntrada()
     continHora()
     selecTurno()
     alert("Servicios disponibles")
     mostrarServicio()
+    selectServicio () 
 }
 
 alert(ingNombre + ", Bienvenido a la barberia \nAtendemos de Martes a Sabados de 14 a 21 HS")
-Turno()
-alert(ingNombre + ", Gracias por solicitar su turno")
+turno()
+
 
 
