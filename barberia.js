2bd3a3b8dfe006ee85c8f80cfa7655d062d1691c
Nicolas San Miguel <nicolassanmiguel3@gmail.com>
2022-07-04 20:15:59 -0300

    barberia.js

--- barberia.js
+++ barberia.js
@@ -57,7 +57,7 @@ function listaPrecios(){
     menuEntrada()
 }
 
-class nuevoCliente {
+class NuevoCliente {
     constructor (nombre,apellido){
         this.nombre = nombre.toUpperCase()
         this.apellido = apellido.toUpperCase()
